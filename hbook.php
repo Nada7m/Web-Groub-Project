6aecc56f06cd11f8551acc0381426aa9f1842362
DALIA <ddmq11@gmail.com>
2025-05-02 11:29:22 +0300

    رجعت التعديلات
    صفحة الحجز html.php رجعت الأشياء اللي انمسحت

--- hbook.php
+++ hbook.php
@@ -4,9 +4,8 @@ $con = mysqli_connect("localhost", "root", "", "Soland");
 if (!$con) {
     die('Cannot connect!: ' . mysqli_connect_error());
 }
-
-$sql = "INSERT INTO hbooking (hotel, bookingdate, roomType, phone) 
-        VALUES ('$_POST[hotel]', '$_POST[date]', '$_POST[roomType]', '$_POST[phone]')";
+$sql = "INSERT INTO bookings (name, hotel, booking_date, room_type, phone, email) 
+        VALUES ('$_POST[name]', '$_POST[hotel]', '$_POST[booking_date]', '$_POST[room_type]', '$_POST[phone]', '$_POST[email]')";
 
 if (!mysqli_query($con, $sql)) {
     die('Cannot add: ' . mysqli_error($con));
